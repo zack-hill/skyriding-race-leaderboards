0d74ae27ab68a223b535dfa6ff6f86eb44f13752
Zack Hill <zackary.t.hill@gmail.com>
2022-12-10 18:19:41 -0500

    Fix copy script

--- CopyFilesToAddonsDirectory.ps1
+++ CopyFilesToAddonsDirectory.ps1
@@ -1,2 +1,2 @@
 
-Copy-Item -Path "DragonridingRaceLeaderboards" -Destination "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\DragonridingRaceLeaderboards" -Recurse
+Copy-Item -Path "DragonridingRaceLeaderboards\" -Destination "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\" -Recurse -Force
