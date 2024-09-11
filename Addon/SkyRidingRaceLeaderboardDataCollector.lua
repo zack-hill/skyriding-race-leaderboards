2201f2c68f1fae127939f21d2130315ba5bb3022
Zack Hill <zackary.t.hill@gmail.com>
2024-09-10 20:36:38 -0400

    Clean up addon and add event hooks

--- Addon/SkyRidingRaceLeaderboardDataCollector.lua
+++ Addon/SkyRidingRaceLeaderboardDataCollector.lua
@@ -1,27 +1,11 @@
 local addonName, addonData = ...
 
-AppName = "Sky Riding Race Leaderboard Data Collector"
-
--- local f = CreateFrame(AppName)
-
--- function f:OnEvent(event, ...)
--- 	self[event](self, event, ...)
--- end
-
--- function f:ADDON_LOADED(event, addOnName)
--- 	print(event, addOnName)
--- end
-
--- function f:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
--- 	print(event, isLogin, isReload)
--- end
-
 function SaveRaceData()
     local _, battleTag = BNGetInfo()
     local characterName = UnitName("player")
     local characterTimeData = {}
 
-    for k, currencyId in ipairs(addonData.raceDataCurrencyIds) do
+    for _, currencyId in ipairs(addonData.raceDataCurrencyIds) do
         local quantity = C_CurrencyInfo.GetCurrencyInfo(currencyId).quantity
         if quantity ~= 0 then
             characterTimeData[currencyId] = quantity
@@ -32,9 +16,14 @@ function SaveRaceData()
     DataCollectorDB[battleTag][characterName] = characterTimeData
 end
 
--- f:RegisterEvent("ADDON_LOADED")
--- f:RegisterEvent("PLAYER_ENTERING_WORLD")
--- f:SetScript("OnEvent", f.OnEvent)
+local function OnEvent(self, event, ...)
+    SaveRaceData()
+end
+
+local frame = CreateFrame("FRAME", addonName);
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+frame:RegisterEvent("PLAYER_LEAVING_WORLD");
+frame:SetScript("OnEvent", OnEvent);
 
 SLASH_SaveRaceData1 = '/srdc';
 function SlashCmdList.SaveRaceData(msg, editBox)
