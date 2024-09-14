b344d8f1052ea65027c596c5670441de5b920234
Mark Tilton <mark.tilton.a@gmail.com>
2024-09-14 10:14:37 -0400

    Format times to be human readable

--- webapp/main.py
+++ webapp/main.py
@@ -10,6 +10,9 @@ bootstrap = Bootstrap5(app)
 def index():
     storage = Storage()
     race_times = storage.get_all_races()
+    for race_time in race_times:
+        time_disp = format_time(race_time.time_ms)
+        race_time.time_disp = time_disp
     return render_template("index.html", race_times=race_times)
 
 
@@ -18,6 +21,9 @@ def race_leaderboard():
     race_id = request.args.get("race_id")
     storage = Storage()
     race_times = storage.get_race_times(race_id)
+    for race_time in race_times:
+        time_disp = format_time(race_time.time_ms)
+        race_time.time_disp = time_disp
     return render_template("leaderboard.html", race_id=race_id, race_times=race_times)
 
 
@@ -35,3 +41,16 @@ def upload_data():
             storage.update_time(battle_tag, race_id, int(time_ms), character_name)
     storage.commit()
     return "", 200
+
+
+def format_time(time_ms) -> str:
+    time_s = time_ms / 1000
+    hours, rem = divmod(time_s, 3600)
+    minutes, seconds = divmod(rem, 60)
+    result_str = ""
+    if hours > 0:
+        result_str += f"{int(hours)}:"
+    if minutes > 0:
+        result_str += f"{int(minutes)}:"
+    result_str += f"{round(seconds, 3):.3f}"
+    return result_str
