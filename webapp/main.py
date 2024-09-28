50182396004b945ea97c75ea55a7bbfb3754a3e5
Zack Hill <zackary.t.hill@gmail.com>
2024-09-28 08:12:28 -0400

    Group courses by race name

--- webapp/main.py
+++ webapp/main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, asdict
+from itertools import groupby
 import math
 from flask import Flask, request, render_template, redirect
 from flask_bootstrap import Bootstrap5
@@ -13,7 +14,6 @@ all_course_info = storage.get_all_course_info()
 APP_DOWNLOAD_LINK = "https://www.dropbox.com/scl/fi/xtld63x64ipluti7bqm22/SkyridingRaceLeaderboardsCompanionApp-1.0.2.0-Setup.exe?rlkey=dml108tbhxhgytgrvlo6dgz2p&st=vu19hsv8&dl=1"
 ADDON_DOWNLOAD_LINK = "https://www.dropbox.com/scl/fi/cc6qw3nv5ylh8j4e41wqd/Addon0.2.zip?rlkey=k03ysa542zphmihixo0ev7r9f&st=g4rl4ozm&dl=1"
 
-
 @dataclass
 class CourseRecord:
     user_id: str
@@ -21,10 +21,13 @@ class CourseRecord:
     course_id: str
     time_disp: str
     character_name: str
-    race_name: str
     course_type: str
     quest_id: int
 
+@dataclass
+class RaceInfo:
+    race_name: str
+    course_records: list[CourseRecord]
 
 @dataclass
 class CourseTimeDisplay:
@@ -47,26 +50,32 @@ class UserScoreDisplay:
 @app.route("/")
 def index():
     storage = Storage()
-    course_times = storage.get_all_course_records()
-    course_records: list[CourseRecord] = []
-    for course_time in course_times:
-        course_info = all_course_info.get(course_time.course_id)
-        if course_info is None or course_info.race_name == "":
-            continue
-        user_name = course_time.user_id.split("#")[0]
-        course_record = CourseRecord(
-            user_id=course_time.user_id,
-            user_name=user_name,
-            course_id=course_time.course_id,
-            time_disp=format_time(course_time.time_ms),
-            character_name=course_time.character_name,
-            race_name=course_info.race_name,
-            course_type=course_info.course_type,
-            quest_id=course_info.quest_id,
-        )
-        course_records.append(course_record)
-    course_records.sort(key=lambda x: x.race_name)
-    return render_template("index.html", course_records=course_records)
+    all_course_times = storage.get_all_course_records()
+    races: list[RaceInfo] = []
+    for race_name, course_times in groupby(all_course_times, lambda x: all_course_info[x.course_id].race_name):
+        race_info = RaceInfo(
+            race_name=race_name,
+            course_records=[],
+        )        
+        for course_time in course_times:
+            course_info = all_course_info.get(course_time.course_id)
+            if course_info is None or course_info.race_name == "":
+                continue
+            user_name = course_time.user_id.split("#")[0]
+            course_record = CourseRecord(
+                user_id=course_time.user_id,
+                user_name=user_name,
+                course_id=course_time.course_id,
+                time_disp=format_time(course_time.time_ms),
+                character_name=course_time.character_name,
+                course_type=course_info.course_type,
+                quest_id=course_info.quest_id,
+            )
+            race_info.course_records.append(course_record)
+        races.append(race_info)
+    races.sort(key=lambda x: x.race_name)
+
+    return render_template("index.html", races=races)
 
 
 @app.route("/course-leaderboard", methods=["GET"])
